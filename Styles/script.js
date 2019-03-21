4b579bc863c387ca43056db0beb80861a2cc95c8
Emma Paris <eparis@atlassian.com>
2019-03-21 14:54:17 -0600

    Subida inicial Pagina Web

---
+++ Styles/script.js
@@ -0,0 +1,37 @@
+$(function () {
+    $("#includedMenu").load("../Pages/menu.html");
+});
+
+(function ($) {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 500) {
+            $(".navbar").addClass("navbar-top-fade");
+        } else {
+            $(".navbar").removeClass("navbar-top-fade");
+        }
+        if (scroll >= 700) {
+            $(".lazy-1").addClass("lazy-1-1");
+        } else {
+            $(".lazy-1").removeClass("lazy-1-1");
+        }
+        if (scroll >= 900) {
+            $(".lazy-2").addClass("lazy-2-1");
+        } else {
+            $(".lazy-2").removeClass("lazy-2-1");
+        }
+
+    });
+})(jQuery);
+
+
+
+window.addEventListener('load', function () {
+    var allimages = document.getElementsByTagName('img');
+    for (var i = 0; i < allimages.length; i++) {
+        if (allimages[i].getAttribute('data-src')) {
+            allimages[i].setAttribute('src', allimages[i].getAttribute('data-src'));
+        }
+    }
+}, false)
